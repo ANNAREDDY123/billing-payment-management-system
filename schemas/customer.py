9017ff9a0714bc44fb46535ac1f90eec7c23a864
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-19 11:41:42 +0530

    Create customer.py

---
+++ schemas/customer.py
@@ -0,0 +1,25 @@
+from pydantic import (
+    BaseModel,
+    EmailStr
+)
+
+
+class CustomerCreate(BaseModel):
+
+    name: str
+
+    email: EmailStr
+
+    phone: str
+
+
+class CustomerResponse(
+    CustomerCreate
+):
+
+    id: int
+
+    is_active: bool
+
+    class Config:
+        from_attributes = True
